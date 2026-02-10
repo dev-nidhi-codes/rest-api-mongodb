7dad0ff8c3b7cf99680a740883242515ff4d4c69
dev-nidhi-codes <archanacp09@gmail.com>
2026-02-10 23:10:34 +0530

    Create productController.js file with product CRUD operations.

---
+++ controllers/productController.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const Product = require('../models/Product');
+
+// Create a new product
+exports.createProduct = async (req, res) => {
+    try {
+        const product = new Product(req.body);
+        await product.save();
+        res.status(201).send(product);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+// Read a product by ID
+exports.getProduct = async (req, res) => {
+    try {
+        const product = await Product.findById(req.params.id);
+        if (!product) {
+            return res.status(404).send();
+        }
+        res.send(product);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
+
+// Update a product by ID
+exports.updateProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
+        if (!product) {
+            return res.status(404).send();
+        }
+        res.send(product);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+};
+
+// Delete a product by ID
+exports.deleteProduct = async (req, res) => {
+    try {
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if (!product) {
+            return res.status(404).send();
+        }
+        res.send(product);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+};
